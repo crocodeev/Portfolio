94b559a31dfd5ff31618c5bb07e7aa7ef445efa5
crocodeev <svin862003@mail.ru>
2023-01-29 15:49:08 +0300

    fix bew game type2

--- projects/FifteenGame/components/NewGame.tsx
+++ projects/FifteenGame/components/NewGame.tsx
@@ -1,6 +1,6 @@
 import styles from "../../../styles/FifteenGame.module.css"
 
-const NewGame = (props: { callback: (event: React.MouseEvent<HTMLButtonElement> | undefined) => void }) => {
+const NewGame = (props: { callback: any }) => {
 
     return(<button
     onClick={props.callback}
